c95410bc6d6e383e3271444818741dae7385daad
Bao Tran Ha <hbtran2004tnct@gmail.com>
2024-09-29 00:58:23 +0700

    finish profile screen

--- App.js
+++ App.js
@@ -1,13 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { Provider } from 'react-redux'; 
+import { useFonts } from 'expo-font';
+import profileScreen from './src/screens/profileScreen'; 
 
+const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    OpenSans: require('./assets/fonts/OpenSans-Regular.ttf'),
+    OpenSansBold: require('./assets/fonts/OpenSans-Bold.ttf'),
+    OpenSansSemiBold: require('./assets/fonts/OpenSans-SemiBold.ttf')
+  });
+
+ 
   return (
-    <View style={styles.container}>
-      <Text>!!!</Text>
-      <StatusBar style="auto" />
-    </View>
+    
+      <NavigationContainer >
+      
+          <StatusBar />
+          <GestureHandlerRootView>
+            <Stack.Navigator initialRouteName="profileScreen" > 
+              <Stack.Screen name="profileScreen" component={profileScreen} options={{ headerShown: false }} />
+            </Stack.Navigator>
+          </GestureHandlerRootView>
+     
+      </NavigationContainer>
+ 
   );
 }
 
---
+++ assets/icons/plus-icon.js
@@ -0,0 +1,3 @@
+export default xml = `<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M6.275 9.425H0V6.275H6.275V0H9.425V6.275H15.7V9.425H9.425V15.7H6.275V9.425Z" fill="#738F81"/>
+</svg>`
